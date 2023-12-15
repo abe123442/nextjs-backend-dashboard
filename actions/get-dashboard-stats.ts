a91a5d11b12a2e92a6ff80c39241d8efe30f4f78
Abraham <ae248abraham@gmail.com>
2023-12-15 15:41:48 +1100

    Completed dashboard ui

---
+++ actions/get-dashboard-stats.ts
@@ -0,0 +1,94 @@
+import prismadb from "../lib/prismadb"
+
+export const getStoreAllDetails = async (storeId: string) => {
+  return await prismadb.store.findUnique({
+    where: {
+      id: storeId
+    },
+    include: {
+      orders: {
+        include: {
+          orderItems: {
+            include: {
+              product: true,
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+type StoreDetails = NonNullable<Awaited<ReturnType<typeof getStoreAllDetails>>>
+
+export const getStoreRevenue = (store: StoreDetails) => {
+  return store.orders.reduce(
+    (total, currentOrder) => {
+      let totalOrderCost = 0
+
+      if (currentOrder.isPaid) {
+        const itemPrices = currentOrder.orderItems.map(({ product }) => product.price)
+        totalOrderCost = itemPrices.reduce((total, currentPrice) => total + currentPrice.toNumber(), 0)
+      }
+
+      return total + totalOrderCost
+    },
+    0
+  )
+}
+
+export const getSalesCount = (store: StoreDetails) => {
+  return store.orders.filter(({ isPaid }) => isPaid).length
+}
+
+export const getStockCount = async (storeId: string) => {
+  return await prismadb.product.count({
+    where: {
+      storeId,
+      isArchived: false,
+    },
+  })
+}
+
+interface GraphData {
+  name: string
+  total: number
+}
+
+export const getGraphRevenue = async (storeId: string) => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        }
+      }
+    }
+  })
+
+  const monthlyRevenue: { [key: number]: number } = {}
+
+  for (const order of paidOrders) {
+    const month = order.createdAt.getMonth()
+    let revenueForOrder = 0
+
+    for (const item of order.orderItems) {
+      revenueForOrder += item.product.price.toNumber()
+    }
+
+    monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder
+  }
+
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  const graphData: GraphData[] = months.map(name => ({ name, total: 0 }))
+
+  for (const month in monthlyRevenue) {
+    graphData[parseInt(month)].total = monthlyRevenue[parseInt(month)]
+  }
+
+  return graphData
+}
--- app/(dashboard)/[storeid]/(routes)/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/page.tsx
@@ -1,20 +1,92 @@
-import prismadb from "@/lib/prismadb"
+import { CreditCard, DollarSign, Package } from "lucide-react"
+
+import { formatPrice } from "@/lib/utils"
+import { getGraphRevenue, getSalesCount, getStockCount, getStoreAllDetails, getStoreRevenue } from "@/actions/get-dashboard-stats"
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Heading } from "@/components/ui/heading"
+import { Separator } from "@/components/ui/separator"
+import Overview from "@/components/standalone/overview"
 
 interface DashboardPageProps {
   params: { storeId: string }
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  })
+  const store = await getStoreAllDetails(params.storeId)
+
+  if (!store) {
+    return null
+  }
+
+  const totalRevenue = getStoreRevenue(store)
+  const salesCount = getSalesCount(store)
+  const stockCount = await getStockCount(store.id)
+  const graphRevenue = await getGraphRevenue(store.id)
 
   return (
-    <div className="p-4">
-      <p>Active Store ID: {store?.id}</p>
-      <p>Active Store Name: {store?.name}</p>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description={`${store?.name} Overview`} />
+        <Separator />
+
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+
+            <CardContent>
+              <div className="text-3xl font-bold">
+                {formatPrice(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Sales
+              </CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+
+            <CardContent>
+              <div className="text-3xl font-bold">
+                +{salesCount}
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Products In Stock
+              </CardTitle>
+              <Package className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+
+            <CardContent>
+              <div className="text-3xl font-bold">
+                {stockCount}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
